11754e11d929ff38180418fc260b5ecdc396ea24
chandanashree <chandanashree7@gmail.com>
2016-04-10 16:44:58 -0700

    Modified order of method call in main

--- TheMotivator.java
+++ TheMotivator.java
@@ -11,8 +11,9 @@ public class TheMotivator{
 	}
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
-		tm.printMessage(60);
 		tm.printScore(50);
+		tm.printMessage(60);
+		
 	}
 	public void printScore(int score){
 		System.out.println("Score : "+score);
