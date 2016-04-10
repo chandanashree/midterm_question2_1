8bb8d72d40127596123fad8e54d8e2b58ca5c33b
chandanashree <chandanashree7@gmail.com>
2016-04-10 16:40:28 -0700

    Modified feedback function name to printMessage

--- TheMotivator.java
+++ TheMotivator.java
@@ -1,5 +1,5 @@
 public class TheMotivator{
-	public void feedback(int score) {
+	public void prinntMessage(int score) {
 		if (score == 100)
 			System.out.println("You're awesome");
 		else if (score > 90)
@@ -11,7 +11,7 @@ public class TheMotivator{
 	}
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
-		tm.feedback(60);
+		tm.printMessage(60);
 		tm.printScore(50);
 	}
 	public void printScore(int score){
